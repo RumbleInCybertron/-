7d2d549ed4a7fd4a3021dff4512212ce34dbc121
Derek Haus <rumbleincybertron@gmail.com>
2018-08-17 16:00:00 +0900

    Click Mouse to Move

---
+++ Assets/Scripts/RayCursor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCursor : MonoBehaviour
+{
+
+    // Cached component references
+    CameraRaycaster myCameraRaycast;
+
+	// Use this for initialization
+	void Start()
+    {
+        myCameraRaycast = GetComponent<CameraRaycaster>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+      //  print(myCameraRaycast.layerHit);
+    }
+}
