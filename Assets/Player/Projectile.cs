5ec7b8175e697f7d9c9341fda0b265853fcae0f2
Derek Haus <rumbleincybertron@gmail.com>
2018-09-06 22:31:06 +0900

    IDamageable Interface and Projectile Class
    Projectile class simply detects any and all triggers of its collider by other objects in the game.

---
+++ Assets/Player/Projectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+	// Use this for initialization
+	void Start()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update()
+    {
+       
+	}
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        print("Projectile hit: " + collider.gameObject);
+    }
+
+
+}
