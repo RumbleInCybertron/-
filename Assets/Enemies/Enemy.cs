3b691c5ccd855999ac4d8ee90454eebf141f98df
Derek Haus <rumbleincybertron@gmail.com>
2018-09-07 11:31:15 +0900

    Enemy Move and Attack Spheres
    Using gizmos, re-added a customizable attack sphere in yellow and added a customizable chase sphere in blue.

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -7,6 +7,7 @@ public class Enemy : MonoBehaviour, IDamageable
 {
     [SerializeField] float maxHealthPoints = 100f;
     [SerializeField] float attackRadius = 4f;
+    [SerializeField] float chaseRadius = 10f;
 
     float currentHealthPoints = 100f;
     AICharacterControl aiCharacterControl = null;
@@ -30,11 +31,28 @@ public class Enemy : MonoBehaviour, IDamageable
         float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
         if(distanceToPlayer <= attackRadius)
         {
+            print(gameObject.name + " attacking player");
+            // TODO spawn projectile
+        }
+
+        if (distanceToPlayer <= chaseRadius)
+        {
             aiCharacterControl.SetTarget(player.transform);
         }
         else
         {
             aiCharacterControl.SetTarget(transform);
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Draw attack sphere
+        Gizmos.color = new Color(0f, 255f, 0f, .5f);
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
+
+        // Draw move sphere
+        Gizmos.color = new Color(0f, 0f, 255f, .5f);
+        Gizmos.DrawWireSphere(transform.position, chaseRadius);
     }
 }
